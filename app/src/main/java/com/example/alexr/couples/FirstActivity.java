19cb8b458cb52caa3f7b951d3e44e785f26876a9
Alexr <arivass90@elpuig.xeill.net>
2019-03-06 00:18:51 +0100

    Firebase conection prueba

--- app/src/main/java/com/example/alexr/couples/FirstActivity.java
+++ app/src/main/java/com/example/alexr/couples/FirstActivity.java
@@ -9,15 +9,78 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.Arrays;
 
 public class FirstActivity extends AppCompatActivity {
 
     ImageView img;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        findViewById(R.id.BtnLoginGoogle).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+        signIn();
+        }
+        });
+
+        findViewById(R.id.BtnLogin).setOnClickListener( new View.OnClickListener() {
+         @Override
+         public void onClick(View w) {
+          Toast toast = Toast.makeText(FirstActivity.this, "Login realizado con exito.\n" +
+          "Bienvenido a Cuples!", Toast.LENGTH_LONG);
+          toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+          toast.show();
+          Intent intent = new Intent(FirstActivity.this, MenuActivity.class);
+          startActivity(intent);
+        }
+        }
+        );
+
+        findViewById(R.id.BtnRegistro).setOnClickListener(
+        new View.OnClickListener() {
+
+         @Override
+         public void onClick(View w) {
+         Intent intent = new Intent(FirstActivity.this, RegisterActivity.class);
+         startActivity(intent); }
+         }
+         );
+
+        comeIn(); }
+
+    void comeIn(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            startActivity(new Intent(this, HeaderGoogle.class));
+            finish();
+        }
+    }
+
+    void signIn(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                 .createSignInIntentBuilder()
+                 .setAvailableProviders(Arrays.asList(
+                 new AuthUI.IdpConfig.GoogleBuilder().build()))
+                .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                comeIn(); }
+        }}
 
         {
             img = findViewById(R.id.Logo);
@@ -25,40 +88,10 @@ public class FirstActivity extends AppCompatActivity {
             .load(R.drawable.logocuples_png)
             .into(img);
         }
-            findViewById(R.id.BtnLogin).setOnClickListener( new View.OnClickListener() {
 
-                        @Override
-
-                        public void onClick(View w) {
-                            Toast toast = Toast.makeText(FirstActivity.this, "Login realizado con exito.\nBienvenido a Cuples!", Toast.LENGTH_LONG);
-                            toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
-                            toast.show();
-                            Intent intent = new Intent(FirstActivity.this, MenuActivity.class);
-                            startActivity(intent);
-                            }
-                            }
-            );
-
-
-            findViewById(R.id.BtnRegistro).setOnClickListener(
-                    new View.OnClickListener() {
-
-                        @Override
-                        public void onClick(View w) {
-                            Intent intent = new Intent(FirstActivity.this, RegisterActivity.class);
-                            startActivity(intent);
-                            }
-                            }
-            );
-        }
         @Override
         public boolean onOptionsItemSelected (MenuItem item){
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
             int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
             if (id == R.id.action_settings) {
                 return true;
             }
