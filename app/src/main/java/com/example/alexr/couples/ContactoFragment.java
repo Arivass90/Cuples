d7560e44ab75d3a66cf33b49568ccbf18f81c9db
Alexr <arivass90@elpuig.xeill.net>
2019-04-09 03:30:45 +0200

    MAPS OK

--- app/src/main/java/com/example/alexr/couples/ContactoFragment.java
+++ app/src/main/java/com/example/alexr/couples/ContactoFragment.java
@@ -1,8 +1,15 @@
 package com.example.alexr.couples;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +17,28 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ContactoFragment extends Fragment {
+public class ContactoFragment extends Fragment implements OnMapReadyCallback{
 
     ImageView img;
     ImageView contacto;
-    ImageView mapa;
 
     public ContactoFragment(){
     }
+
+    GoogleMap map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,13 +55,31 @@ public class ContactoFragment extends Fragment {
         .load(R.drawable.imagencontacto)
         .into(contacto);
 
-        mapa= rootView.findViewById(R.id.mapacontacto);
-        Glide.with(this)
-                .load(R.drawable.mapacontacto)
-                .into(mapa);
-
         return rootView;
+    }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        UiSettings uiSettings = map.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+
+        LatLng  pp= new LatLng(41.401473,2.145112);
+        map.addMarker(new MarkerOptions().position(pp).title("Cuple's Club"));
+
+        float zoomlevel = 18;
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pp,zoomlevel));
+    }
 
     }
-}
