ec1da566cba5a10552112aa68f0102ff77255591
amartimez <amartimez@elpuig.xeill.net>
2019-05-31 12:23:47 +0200

    finalizado layout calendario

---
+++ app/src/main/java/com/example/alexr/couples/view/activity/TestActivity.java
@@ -0,0 +1,18 @@
+package com.example.alexr.couples.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alexr.couples.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+
+    }
+}
